4598aa461b768eb0d1aae0a2f1b2602c97f4e0bb
beeinger <sulecki.maciek@gmail.com>
2019-10-24 12:17:22 +0200

    tests

--- src/routes/facebook.js
+++ src/routes/facebook.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import https from "https";
+import FB from "fb";
 const router = Router();
 var user;
 var data;
@@ -11,7 +11,14 @@ router.post("/", async (req, res) => {
       userID: info.userID,
       accessToken: info.accessToken
     };
-    importFacebook(user.userID, user.accessToken, done);
+    //here
+    FB.api(
+      "me",
+      { fields: ["id", "name"], access_token: user.accessToken },
+      function(res) {
+        console.log(res);
+      }
+    );
   }
   return res.send(info);
 });
@@ -31,39 +38,5 @@ router.get("/friends", async (req, res) => {
     return res.send("Try logging in first");
   }
 });
-
-function done(parcel) {
-  console.log("callback", parcel);
-}
-
-function importFacebook(userID, accessToken, cb) {
-  var options = {
-    host: "graph.facebook.com",
-    port: 443,
-    path: "/v2.9/" + userID + "/taggable_friends?access_token=" + accessToken, //apiPath example: '/me/friends'
-    method: "GET"
-  };
-
-  var buffer = ""; //this buffer will be populated with the chunks of the data received from facebook
-  var request = https.get(options, function(result) {
-    result.setEncoding("utf8");
-    result.on("data", function(chunk) {
-      buffer += chunk;
-    });
-
-    result.on("end", function() {
-      data = buffer;
-      console.log(buffer);
-      cb();
-    });
-  });
-
-  request.on("error", function(e) {
-    console.log("error from facebook.getFbData: " + e.message);
-    cb();
-  });
-
-  request.end();
-}
 
 export default router;
